aef4665a2d16ab4f45bce96d28c3a04cd8a8be69
siddid <siddidsoni@gmail.com>
2022-02-23 00:02:39 +0530

    docker added

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,6 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-
 app.listen(port, () => {
   console.log(`iNotebook backend listening at http://localhost:${port}`)
 })
